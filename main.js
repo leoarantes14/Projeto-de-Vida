35a988798cd73229f3639e13d787302af5bebc15
leoarantes14 <leonardo.arantes.miguel@escola.pr.gov.br>
2026-05-18 07:44:41 -0300

    return

--- main.js
+++ main.js
@@ -23,14 +23,14 @@ contadores[0].textContent = calculaTempo(tempoObjetivo1);
 function calculaTempo(tempoObjetivo){
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
-    let segundos = Math.floor(tempoFinal/1000);
-    let minutos = Math.floor(segundos/60);
-    let horas = Math.floor(minutos/60);
-    let dias = Math.florr(horas/24);
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.florr(horas / 24);
 
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
-    
-    return dias + " dias "  + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
+
+    return (dias + " dias "  + horas + " horas " + minutos + " minutos " + segundos + " segundos ");
 }
