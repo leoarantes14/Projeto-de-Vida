c66a0d2ef1687ef72e6c021dc93c2f73700b0291
leoarantes14 <leonardo.arantes.miguel@escola.pr.gov.br>
2026-05-18 08:26:03 -0300

    alterações

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ function calculaTempo(tempoObjetivo){
   let segundos = Math.floor(tempoFinal / 1000);
   let minutos = Math.floor(segundos / 60);
     let horas = Math.floor(minutos / 60);
-    let dias = Math.florr(horas / 24);
+    let dias = Math.floor(horas / 24);
     
     segundos %= 60;
     minutos %= 60;
@@ -49,5 +49,5 @@ function calculaTempo(tempoObjetivo){
       atualizaCronometro();
       setInterval(atualizaCronometro,1000);
     }
-    
+
     comecaCronometro();
